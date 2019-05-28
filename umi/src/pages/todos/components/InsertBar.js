0f7ed7299e63d14f9ba5fbb92f4a16acd7116fce
Ethan <ls4int@gmail.com>
2019-05-28 12:05:07 +0800

    refactor InsertBar to function

--- umi/src/pages/todos/components/InsertBar.js
+++ umi/src/pages/todos/components/InsertBar.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
-export default class InsertBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.addTodoAsync(e.target[0].value);
-    e.target.reset();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="New Todo" />
-      </form>
-    );
-  }
+export default function InsertBar(props) {
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        props.addTodoAsync(e.target[0].value);
+        e.target.reset();
+      }}
+    >
+      <input type="text" placeholder="New Todo" />
+    </form>
+  );
 }
