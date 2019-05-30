42725cb8d9963281d45f1e94332a8406b9352952
Ethan <ls4int@gmail.com>
2019-05-30 15:51:18 +0800

    try layout

--- umi/src/layouts/index.js
+++ umi/src/layouts/index.js
@@ -3,7 +3,15 @@ import styles from './index.css';
 function BasicLayout(props) {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      <h1 className={styles.title}>Another day, Another dollor.</h1>
+      <ul className={styles.nav}>
+        <li>
+          <a href="/"> index</a>
+        </li>
+        <li>
+          <a href="/todos"> Todos</a>
+        </li>
+      </ul>
       {props.children}
     </div>
   );
