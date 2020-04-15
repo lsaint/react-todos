1848f2ae031cee8b1516407de2388a8444e132fa
Ethan <ls4int@gmail.com>
2020-04-15 17:39:33 +0800

    umi ui

--- umi/config/config.js
+++ umi/config/config.js
@@ -14,4 +14,13 @@ export default {
     // 使用命令行传入>>> API_URL=http://localhost yarn start
     //'process.env.API_URL': process.env.API_URL,
   },
+
+  // 配置式路由，页面已按约定式，写上去是因为umi ui不支持约定式路由
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index.js',
+      routes: [{ path: '/', component: 'index' }, { path: '/todos', component: 'todos' }],
+    },
+  ],
 };
